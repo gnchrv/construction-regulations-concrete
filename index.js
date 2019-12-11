7e2d31fa898a7d19de5ce129aab1f4da215fd6fd
Misha Goncharov <forsomeletters@gmail.com>
2019-12-11 19:06:05 +0300

    Rb, Rbt and Eb for conventional concretes

---
+++ index.js
@@ -0,0 +1,169 @@
+// All values are given in SI
+
+module.exports = {
+  'B3.5': {
+    conventional: {
+      Rb: 2.1e6,
+      Rbt: .26e6,
+      Eb: {
+        normal: 7e9
+      }
+    }
+  },
+  'B5': {
+    conventional: {
+      Rb: 2.8e6,
+      Rbt: .37e6,
+      Eb: {
+        normal: 10e9
+      }
+    }
+  },
+  'B7.5': {
+    conventional: {
+      Rb: 4.5e6,
+      Rbt: .48e6,
+      Eb: {
+        normal: 13.5e9
+      }
+    }
+  },
+  'B10': {
+    conventional: {
+      Rb: 6.0e6,
+      Rbt: .56e6,
+      Eb: {
+        normal: 15.5e9
+      }
+    }
+  },
+  'B12.5': {
+    conventional: {
+      Rb: 7.5e6,
+      Rbt: .66e6,
+      Eb: {
+        normal: 17.5e9
+      }
+    }
+  },
+  'B15': {
+    conventional: {
+      Rb: 8.5e6,
+      Rbt: .75e6,
+      Eb: {
+        normal: 19.5e9,
+        autoclave: 16.5e9
+      }
+    }
+  },
+  'B20': {
+    conventional: {
+      Rb: 11.5e6,
+      Rbt: .90e6,
+      Eb: {
+        normal: 22e9,
+        autoclave: 18e9
+      }
+    }
+  },
+  'B25': {
+    conventional: {
+      Rb: 14.5e6,
+      Rbt: 1.05e6,
+      Eb: {
+        normal: 24e9,
+        autoclave: 19.5e9
+      }
+    }
+  },
+  'B30': {
+    conventional: {
+      Rb: 17e6,
+      Rbt: 1.15e6,
+      Eb: {
+        normal: 26e9,
+        autoclave: 21e9
+      }
+    }
+  },
+  'B35': {
+    conventional: {
+      Rb: 19.5e6,
+      Rbt: 1.30e6,
+      Eb: {
+        normal: 27.5e9,
+        autoclave: 22e9
+      }
+    }
+  },
+  'B40': {
+    conventional: {
+      Rb: 22e6,
+      Rbt: 1.40e6,
+      Eb: {
+        normal: 28.5e9,
+        autoclave: 23e9
+      }
+    }
+  },
+  'B45': {
+    conventional: {
+      Rb: 25e6,
+      Rbt: 1.50e6,
+      Eb: {
+        autoclave: 23.5e9
+      }
+    }
+  },
+  'B50': {
+    conventional: {
+      Rb: 27.5e6,
+      Rbt: 1.60e6,
+      Eb: {
+        autoclave: 24.0e9
+      }
+    }
+  },
+  'B55': {
+    conventional: {
+      Rb: 30e6,
+      Rbt: 1.70e6,
+      Eb: {
+        autoclave: 24.5e9
+      }
+    }
+  },
+  'B60': {
+    conventional: {
+      Rb: 33e6,
+      Rbt: 1.80e6,
+      Eb: {
+        autoclave: 25.0e9
+      }
+    }
+  },
+  'B70': {
+    conventional: {
+      Rb: 37e6,
+      Rbt: 1.90e6,
+    }
+  },
+  'B80': {
+    conventional: {
+      Rb: 41e6,
+      Rbt: 2.10e6,
+    }
+  },
+  'B90': {
+    conventional: {
+      Rb: 44e6,
+      Rbt: 2.15e6,
+    }
+  },
+  'B100': {
+    conventional: {
+      Rb: 47.5e6,
+      Rbt: 2.20e6,
+    }
+  }
+}
